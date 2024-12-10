a86cff9af5db55afc0a0e0d46e03e42a75e9446c
Dahlia-leah <dahlialeah98@gmail.com>
2024-12-10 15:48:05 +0200

    Update balance_interface.py

--- stock_picking_report/iot_handlers/interfaces/balance_interface.py
+++ stock_picking_report/iot_handlers/interfaces/balance_interface.py
@@ -1,5 +1,20 @@
-from odoo.addons.iot.models.iot_device import IoTDeviceDriver
+from odoo.addons.hw_drivers.interface import Interface
 
-class BalanceInterface(IoTDeviceDriver):
-    def read(self):
-        return self.device.call('adam_scale_driver.read')
+class SerialInterface(Interface):
+    connection_type = 'serial'
+
+    def get_devices(self):
+        """
+        Detect devices connected via serial ports and return their details.
+        """
+        # Example device detection logic (replace with actual implementation)
+        devices = {
+            'scale_com5': {
+                'identifier': 'scale_com5',
+                'device_name': 'Adam Scale',
+                'device_type': 'scale',
+                'port': 'COM5',  # Update with the detected COM port
+                'manufacturer': 'Adam',
+            }
+        }
+        return devices
