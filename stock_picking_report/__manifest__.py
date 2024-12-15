8903e2d0f73660b20f7e715794c4e2f8c8455da4
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-15 11:51:20 +0200

    <add>

--- stock_picking_report/__manifest__.py
+++ stock_picking_report/__manifest__.py
@@ -1,13 +1,15 @@
 {
     'name': 'Stock Picking Report',
     'version': '1.0',
-    'license': 'LGPL-3',  # Specify the license here
+    'license': 'LGPL-3',
     'category': 'Inventory',
-    'summary': 'Add a button to print reports in stock picking forms',
-    'depends': ['stock', 'iot'  ,'hw_drivers'],  # This module depends on the stock module
+    'summary': 'Add a button to fetch weight from IoT device and print stock picking reports.',
+    'depends': ['stock', 'iot'],
     'data': [
-        'views/stock_picking_views.xml',  # View modifications (e.g., form view with JS)
-        'reports/stock_picking_report_template.xml',  # Report template
+        'views/stock_picking_views.xml',
+        'reports/stock_picking_report_template.xml',
     ],
     'installable': True,
+    'application': False,
+    'auto_install': False,
 }
