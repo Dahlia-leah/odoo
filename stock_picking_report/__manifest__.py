9f970594a32856aa6078236badcb7e1bbe403be7
Dahlia-leah <dahlialeah98@gmail.com>
2024-12-17 11:07:06 +0200

    Update __manifest__.py

--- stock_picking_report/__manifest__.py
+++ stock_picking_report/__manifest__.py
@@ -8,7 +8,11 @@
     'data': [
         'views/stock_picking_views.xml',
         'reports/stock_picking_report_template.xml',
-    ],
+    ],'assets': {
+        'web.assets_backend': [
+            'your_module/static/src/js/live_weight_bus.js',
+        ],
+    },
     'installable': True,
     'application': False,
     'auto_install': False,
