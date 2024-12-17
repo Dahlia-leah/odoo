9e0322fbaa61b67879f73ee6d67df615993dbd06
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-17 14:52:52 +0200

    <new>

--- custom_live_scale/__manifest__.py
+++ custom_live_scale/__manifest__.py
@@ -2,6 +2,7 @@
     'name': 'Live Scale Integration',
     'version': '1.0',
     'category': 'Tools',
+    'license': 'LGPL-3',
     'summary': 'Integration with Live Scale for Weight Data',
     'description': """
         A custom Odoo module to integrate with a live scale via an API, exposing data on the Odoo system.
