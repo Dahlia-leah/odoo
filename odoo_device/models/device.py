e3870b0310a0b6e863f7ebec8fd0ffe4533dc3b2
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-23 13:00:38 +0200

    <fl

--- odoo_device/models/device.py
+++ odoo_device/models/device.py
@@ -4,8 +4,13 @@ class Device(models.Model):
     _name = 'device'
     _description = 'Device'
 
-    name = fields.Char(string="Device Name", required=True)
-    device_type = fields.Char(string="Device Type")
-    serial_number = fields.Char(string="Serial Number")
-    purchase_date = fields.Date(string="Purchase Date")
-    warranty_expiry = fields.Date(string="Warranty Expiry")
+    name = fields.Char(string='Device Name', required=True)
+    device_type = fields.Selection([
+        ('mobile', 'Mobile'),
+        ('laptop', 'Laptop'),
+        ('tablet', 'Tablet'),
+        ('other', 'Other'),
+    ], string='Device Type', default='other')
+    serial_number = fields.Char(string='Serial Number')
+    purchase_date = fields.Date(string='Purchase Date')
+    warranty_expiry = fields.Date(string='Warranty Expiry')
