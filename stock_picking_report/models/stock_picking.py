2f8f39cc80462053dea7a86b1c74f0c0d078f1ac
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-28 14:48:04 +0200

    sd

--- stock_picking_report/models/stock_picking.py
+++ stock_picking_report/models/stock_picking.py
@@ -69,22 +69,22 @@ class StockMove(models.Model):
 
         except requests.exceptions.RequestException as e:
             self._open_scale_error_wizard(_(f"Error connecting to the scale service: {str(e)}."))
-            
+
     def _open_scale_error_wizard(self, message):
-        """
-         Opens the wizard for handling scale connection errors.
-         """
-        return {
-        'type': 'ir.actions.act_window',
-        'res_model': 'scale.connection.wizard',
-        'view_mode': 'form',
-        'view_type': 'form',
-        'target': 'new',
-        'context': {
-            'default_message': message,
-            'default_stock_move_id': self.id,
-        },
+       """
+       Opens the wizard for handling scale connection errors.
+      """
+       return {
+          'type': 'ir.actions.act_window',
+          'res_model': 'scale.connection.wizard',
+          'view_mode': 'form',
+          'target': 'new',
+          'context': {
+              'default_message': message,
+              'default_stock_move_id': self.id,
+           },
        }
+
 
     def action_print_report(self):
         """
