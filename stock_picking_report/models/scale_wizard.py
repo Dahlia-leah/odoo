d10015ce8a6b313931fbad418982d6b24e52ed52
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-28 16:05:58 +0200

    rev

--- stock_picking_report/models/scale_wizard.py
+++ stock_picking_report/models/scale_wizard.py
@@ -8,14 +8,13 @@ class ScaleConnectionWizard(models.TransientModel):
     _description = 'Scale Connection Error Wizard'
 
     message = fields.Text(string="Message", readonly=True, default="The scale is not connected.")
-    stock_move_id = fields.Many2one('stock.move', string="Stock Move", readonly=True)
 
     def action_print_empty(self):
         """
         Proceed with printing empty data.
         """
         
-        self.stock_move_id.action_force_empty_print()
+        self.action_force_empty_print()
         
 
     def action_cancel(self):
