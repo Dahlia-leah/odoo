ef227adebbd1bc00e746f070ceb0a72cd5962593
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-28 16:02:36 +0200

    xc

--- stock_picking_report/models/scale_wizard.py
+++ stock_picking_report/models/scale_wizard.py
@@ -14,16 +14,9 @@ class ScaleConnectionWizard(models.TransientModel):
         """
         Proceed with printing empty data.
         """
-        self.ensure_one()
-        _logger.info("action_print_empty called for record ID: %s", self.id)
-
-        if self.stock_move_id:
-            _logger.info("Calling action_force_empty_print on stock_move_id: %s", self.stock_move_id.id)
-            self.stock_move_id.action_force_empty_print()
-        else:
-            _logger.warning("No stock_move_id set for this wizard.")
-
-        return {'type': 'ir.actions.act_window_close'}
+        
+        self.stock_move_id.action_force_empty_print()
+        
 
     def action_cancel(self):
         """
