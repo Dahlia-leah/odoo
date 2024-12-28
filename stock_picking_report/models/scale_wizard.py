78be09ca0dce337d6f43288f9ba6e2f0548fed79
@Dahlia-leah <dahlialeah98@gmail.com>
2024-12-28 15:47:58 +0200

    ds

--- stock_picking_report/models/scale_wizard.py
+++ stock_picking_report/models/scale_wizard.py
@@ -1,4 +1,7 @@
 from odoo import models, fields, api, _
+import logging
+
+_logger = logging.getLogger(__name__)  # Logger initialization
 
 class ScaleConnectionWizard(models.TransientModel):
     _name = 'scale.connection.wizard'
@@ -12,12 +15,19 @@ class ScaleConnectionWizard(models.TransientModel):
         Proceed with printing empty data.
         """
         self.ensure_one()
+        _logger.info("action_print_empty called for record ID: %s", self.id)
+
         if self.stock_move_id:
+            _logger.info("Calling action_force_empty_print on stock_move_id: %s", self.stock_move_id.id)
             self.stock_move_id.action_force_empty_print()
+        else:
+            _logger.warning("No stock_move_id set for this wizard.")
+
         return {'type': 'ir.actions.act_window_close'}
 
     def action_cancel(self):
         """
         Cancel the operation and close the wizard.
         """
+        _logger.info("action_cancel called for record ID: %s", self.id)
         return {'type': 'ir.actions.act_window_close'}
