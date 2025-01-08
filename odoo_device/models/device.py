95afb8887d72e777da8124c6a4b7971c35ae04fe
@Dahlia-leah <dahlialeah98@gmail.com>
2025-01-08 11:46:10 +0200

    try

--- odoo_device/models/device.py
+++ odoo_device/models/device.py
@@ -1,8 +1,15 @@
-from odoo import models, fields
+from odoo import models, fields, api, exceptions
 
 class Device(models.Model):
     _name = 'devices.device'
     _description = 'Device'
 
-    name = fields.Char(string='Device Name', required=True)
-    device_id = fields.Char(string='Device ID', required=True, unique=True)
+    name = fields.Char(string='Device Name', required=True, default='Scale')
+    device_id = fields.Char(string='Device ID', required=True, default='1', unique=True)
+
+    @api.model
+    def create(self, vals):
+        raise exceptions.UserError("You cannot create new Device records.")
+
+    def unlink(self):
+        raise exceptions.UserError("You cannot delete Device records.")
