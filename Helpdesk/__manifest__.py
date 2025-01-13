130705a58bb4e31ab41f4aa32cbfd40c38a09d34
@Dahlia-leah <dahlialeah98@gmail.com>
2025-01-13 12:34:59 +0200

    23456

--- Helpdesk/__manifest__.py
+++ Helpdesk/__manifest__.py
@@ -3,7 +3,6 @@
     'version': '1.0',
     'license': 'LGPL-3',
     'summary': 'Filter helpdesk team members and auto-create tasks for tickets',
-    'author': 'Your Name',
     'depends': ['helpdesk', 'project'],
     'data': [
         'security/ir.model.access.csv',
