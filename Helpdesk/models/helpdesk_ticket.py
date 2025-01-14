2ecc1f8d63b89864f0b74f67d0634e7125bdeefc
Dahlia-leah <dahlialeah98@gmail.com>
2025-01-14 22:44:45 +0200

    Update helpdesk_ticket.py

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -4,12 +4,14 @@ class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     assigned_user_id = fields.Many2one(
+        'res.users',
         string="Assigned To",
         domain="[('id', 'in', member_ids)]",
         help="Only members of the Helpdesk Team can be assigned."
     )
 
     member_ids = fields.Many2many(
+        'hr.employee',
         related='team_id.member_ids',
         string="Team Members",
         readonly=True
