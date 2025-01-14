4f6410f92a0a6ef2207e5e8e3babac433b619911
Dahlia-leah <dahlialeah98@gmail.com>
2025-01-14 23:21:22 +0200

    Update helpdesk_ticket.py

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -4,15 +4,25 @@ class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     assigned_user_id = fields.Many2one(
-        'hr.employee',
+         'hr.employee',
         string="Assigned Employee",
         domain="[('id', 'in', member_ids)]",
-        help="Only members of the Helpdesk Team can be assigned."
+        help="Only team members can be assigned."
     )
 
     member_ids = fields.Many2many(
         'hr.employee',
-        related='team_id.member_ids',
         string="Team Members",
-        readonly=True
+        compute="_compute_team_members",
+        store=True
     )
+
+    @api.depends('team_id')
+    def _compute_team_members(self):
+        for ticket in self:
+            if ticket.team_id:
+                analytic_lines = self.env['account.analytic.line'].search([('employee_id', '!=', False)])
+                employee_ids = analytic_lines.mapped('employee_id.id')
+                ticket.member_ids = [(6, 0, employee_ids)]
+            else:
+                ticket.member_ids = [(5,)]  # Clear the field if no team is set
