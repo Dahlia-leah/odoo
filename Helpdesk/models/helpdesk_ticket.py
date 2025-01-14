5d324e0bf6df7280b512db11e6274f6e037f34be
Dahlia-leah <dahlialeah98@gmail.com>
2025-01-14 23:30:13 +0200

    nre

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -1,28 +1,30 @@
 from odoo import api, fields, models
 
+
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
     assigned_user_id = fields.Many2one(
-          'hr.employee',
+        'hr.employee',
         string="Assigned Employee",
-        domain="[('id', 'in', member_ids.ids)]",
+        domain="[('id', 'in', member_ids)]",
         help="Only team members can be assigned."
     )
 
     member_ids = fields.Many2many(
         'hr.employee',
-        related='team_id.member_ids',
         string="Team Members",
-        readonly=True
+        compute="_compute_team_members",
+        store=False,  # This is computed dynamically, no need to store in DB
     )
 
     @api.depends('team_id')
     def _compute_team_members(self):
         for ticket in self:
             if ticket.team_id:
-                analytic_lines = self.env['account.analytic.line'].search([('employee_id', '!=', False)])
+                # Fetch employees linked to the team (with or without users)
+                analytic_lines = self.env['account.analytic.line'].search([])
                 employee_ids = analytic_lines.mapped('employee_id.id')
                 ticket.member_ids = [(6, 0, employee_ids)]
             else:
-                ticket.member_ids = [(5,)]  # Clear the field if no team is set
+                ticket.member_ids = [(5,)]  # Clear field if no team is set
