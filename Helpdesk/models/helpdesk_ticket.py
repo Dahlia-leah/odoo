247d238a0e3ed9f0d76e614184ecfdf6fcdd9bf3
Dahlia-leah <dahlialeah98@gmail.com>
2025-01-15 00:12:32 +0200

    Update helpdesk_ticket.py

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -11,23 +11,14 @@ class HelpdeskTicket(models.Model):
         help="Only team members can be assigned."
     )
 
-    member_ids = fields.Many2many(
-        'hr.employee',  # Model to which the field points (employees)
-        'helpdesk_ticket_employee_rel',  # The name of the relation table
-        'ticket_id',  # Field name for the current model (HelpdeskTicket)
-        'employee_id',  # Field name for the related model (Employee)
+     member_ids = fields.Many2many(
+        'hr.employee',  # Reference to the Employee model
+        'helpdesk_ticket_employee_rel',  # Name of the relationship table
+        'ticket_id',  # Column in the relationship table for this model
+        'employee_id',  # Column in the relationship table for the related model
         string="Team Members",
+        domain=[('active', '=', True)],  # Optional: Only show active employees
         store=True,  # Store the relationship in the database
     )
 
-    @api.depends('team_id')
-    def _compute_team_members(self):
-        for ticket in self:
-            if ticket.team_id:
-                # Fetch employees from account.analytic.line regardless of user association
-                analytic_lines = self.env['account.analytic.line'].search([])
-                employee_ids = analytic_lines.mapped('employee_id.id')
-                ticket.member_ids = [(6, 0, employee_ids)]
-            else:
-                ticket.member_ids = [(5,)]  # Clear if no team is set
-
+   
