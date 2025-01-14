6b492e429cc6d1a08164b22adae54591d31a8496
Dahlia-leah <dahlialeah98@gmail.com>
2025-01-15 00:53:02 +0200

    Update helpdesk_ticket.py

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -3,16 +3,28 @@ from odoo import api, fields, models
 class HelpdeskTicket(models.Model):
     _inherit = "helpdesk.ticket"
 
-    assigned_user_id = fields.Many2one(
-        'res.users',
-        string="Assigned To",
-        domain="[('id', 'in', member_ids.ids)]",
-        help="Only members of the Helpdesk Team can be assigned."
+    member_ids = fields.Many2many(
+        'hr.employee',
+        string="Team Members",
+        related='team_id.member_ids',
+        readonly=True,
+        store=True,
+        help="Team Members assigned to this Helpdesk Team.",
     )
+
+    assigned_user_id = fields.Many2one(
+        'hr.employee',
+        string="Assigned To",
+        domain="[('id', 'in', member_ids)]",
+        help="Only employees who are part of the Helpdesk Team can be assigned."
+    )
+
+    
+class HelpdeskTeam(models.Model):
+    _inherit = "helpdesk.team"
 
     member_ids = fields.Many2many(
         'hr.employee',
         string="Team Members",
-        readonly=True,
-        related='team_id.member_ids',
+        help="Employees who are part of this Helpdesk Team."
     )
