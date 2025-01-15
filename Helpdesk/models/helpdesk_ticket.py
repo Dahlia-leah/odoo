976fba833e668a3585fd66aa383c032f43cb9c72
@Dahlia-leah <dahlialeah98@gmail.com>
2025-01-15 08:54:11 +0200

    hoh

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -5,7 +5,7 @@ class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
 
     # Define the team members field as a many2many relation to the hr.employee model
-    team_member_ids = fields.Many2many(
+    member_ids = fields.Many2many(
         'hr.employee', 
         string='Team Members', 
         domain=[('is_active', '=', True)],  # Only active employees can be part of the team
@@ -15,7 +15,7 @@ class HelpdeskTicket(models.Model):
     assigned_employee_id = fields.Many2one(
         'hr.employee', 
         string='Assigned Team Member',
-        domain="[('id', 'in', team_member_ids)]",  # Limit assignment to team members only
+        domain="[('id', 'in', member_ids)]",  # Limit assignment to team members only
         track_visibility='onchange',
     )
 
