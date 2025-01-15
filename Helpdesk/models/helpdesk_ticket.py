f411b1d5fa846c08f9372589fd74d6c6cb92c274
@Dahlia-leah <dahlialeah98@gmail.com>
2025-01-15 08:48:09 +0200

    sd

--- Helpdesk/models/helpdesk_ticket.py
+++ Helpdesk/models/helpdesk_ticket.py
@@ -1,30 +1,27 @@
-from odoo import api, fields, models
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
 
 class HelpdeskTicket(models.Model):
-    _inherit = "helpdesk.ticket"
+    _inherit = 'helpdesk.ticket'
 
-    member_ids = fields.Many2many(
-        'hr.employee',
-        string="Team Members",
-        related='team_id.member_ids',
-        readonly=True,
-        store=True,
-        help="Team Members assigned to this Helpdesk Team.",
+    # Define the team members field as a many2many relation to the hr.employee model
+    team_member_ids = fields.Many2many(
+        'hr.employee', 
+        string='Team Members', 
+        domain=[('is_active', '=', True)],  # Only active employees can be part of the team
     )
 
-    assigned_user_id = fields.Many2one(
-        'hr.employee',
-        string="Assigned To",
-        domain="[('id', 'in', member_ids)]",
-        help="Only employees who are part of the Helpdesk Team can be assigned."
+    # Override the assigned user field to restrict to team members
+    assigned_employee_id = fields.Many2one(
+        'hr.employee', 
+        string='Assigned Team Member',
+        domain="[('id', 'in', team_member_ids)]",  # Limit assignment to team members only
+        track_visibility='onchange',
     )
 
-    
-class HelpdeskTeam(models.Model):
-    _inherit = "helpdesk.team"
-
-    member_ids = fields.Many2many(
-        'hr.employee',
-        string="Team Members",
-        help="Employees who are part of this Helpdesk Team."
-    )
+    # Optionally, we can create a function to ensure that a task can only be assigned to a team member
+    @api.constrains('assigned_employee_id')
+    def _check_assigned_employee(self):
+        for record in self:
+            if record.assigned_employee_id not in record.team_member_ids:
+                raise ValidationError("The assigned employee must be part of the team.")
